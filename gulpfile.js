c68563c33bd2fb62d4504f770ff53fb831210197
caocaoer <caocaoer1019@163.com>
2016-08-31 18:08:54 +0800

    add

---
+++ gulpfile.js
@@ -0,0 +1,26 @@
+var gulp = require('gulp'),
+    browserSync = require('browser-sync').create(),
+    sass = require('gulp-sass'),
+    reload = browserSync.reload;
+
+gulp.task('serve', function(){
+    browserSync.init({
+        server: {
+            baseURI: ''
+        }
+    });
+    gulp.watch('src/sass/*.scss', ['sass']);
+    gulp.watch('src/*.html').on('change', reload);
+    gulp.watch('src/js/*.js').on('change', reload);
+});
+
+gulp.task('sass', function(){
+    gulp.src('src/sass/*.scss')
+    .pipe(sass({
+        outputStyle: 'compressed'
+    }).on('error', sass.logError))
+    .pipe(gulp.dest('src/css'))
+    .pipe(reload({
+        stream: true
+    }));
+});
