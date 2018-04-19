1d9e838ef04beb2dc84371028a1ebb686316939a
caoyan <caoyan@bbdservice.com>
2018-04-19 09:54:01 +0800

    add

--- src/layouts/frame/Frame.jsx
+++ src/layouts/frame/Frame.jsx
@@ -1,3 +1,4 @@
+import style from './frame.scss';
 import React, { Component } from 'react';
 import Header from '../header/Header';
 import Footer from '../footer/Footer';
@@ -5,9 +6,11 @@ import Footer from '../footer/Footer';
 export default class Frame extends Component {
     render() {
         return (
-            <div>
+            <div className={style.frame}>
                 <Header/>
-                {this.props.children}
+                <div className={style.container}>
+                    {this.props.children}
+                </div>
                 <Footer/>
             </div>
         );
